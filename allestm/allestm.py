070fe01190e752759303c798d2821eb6553d418f
Peter Hönigschmid <hoenigschmid.peter@gmail.com>
2019-11-30 17:32:19 +0100

    Finished predictions (i.e. blending and avg)

--- allestm/allestm.py
+++ allestm/allestm.py
@@ -17,6 +17,7 @@ from allestm.features.categorical import Sequence
 from allestm.features.continuous import Pssm
 
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 import logging
 
@@ -30,13 +31,6 @@ def parse_args(argv):
     parser.add_argument('-a', '--a3m', help='MSA in a3m format.')
     parser.add_argument('-c', '--config', help='Config file.')
     return parser.parse_args(argv)
-
-# ["lstm_", "cnn_", "dcnn_", "rf_", "xgb_"]
-# * 5 folds
-# blending
-# avg
-# x targets
-
 
 def prepare_data(seq, pssm, window=0):
     feature_dfs = []
@@ -77,11 +71,62 @@ def main():
     pssm = pssm_transformer.transform(a3m)
 
     # Cache: cache[FOLD][METHOD][TARGET] = PREDICTIONS.
-    cache = {}
+    results = {}
 
+    do_dl = False
     do_rf = False
     do_xgb = False
-    do_dl = True
+    do_blending = True
+    do_avg = True
+
+    # DL.
+    if do_dl:
+        def run_dl(seq, pssm, model_file, queue):
+            model = keras.models.load_model(model_file)
+            predictions = model.predict([dl_seq_data, dl_pssm_data])
+            queue.put(predictions)
+
+        dl_seq_data_tmp, dl_pssm_data_tmp = (x.to_numpy() for x in prepare_data(seq, pssm, window=0))
+
+        dl_seq_data = np.zeros((1, config['config']['max_length'], len(seq_transformer)))
+        dl_pssm_data = np.zeros((1, config['config']['max_length'], len(seq_transformer)))
+
+        dl_seq_data[0, :dl_seq_data_tmp.shape[0], :dl_seq_data_tmp.shape[1]] = dl_seq_data_tmp
+        dl_pssm_data[0, :dl_pssm_data_tmp.shape[0], :dl_pssm_data_tmp.shape[1]] = dl_pssm_data_tmp
+
+        for method in ['cnn', 'dcnn', 'lstm']:
+            for target_group in config['methods'][method]:
+                log.debug(f'Target group {target_group}.')
+
+                for model_config in config['methods'][method][target_group]:
+                    log.debug(f'Model config: {model_config}.')
+                    queue = multiprocessing.SimpleQueue()
+
+                    model_file = f'models/{model_config["model_file"]}'
+                    log.debug(f'Loading model file {model_file} for targets {model_config["targets"]}.')
+
+                    # Necessary to free memory after prediction.
+                    p = multiprocessing.Process(target=run_dl, args=(dl_seq_data, dl_pssm_data, model_file, queue))
+                    p.start()
+                    p.join()
+
+                    predictions = queue.get()
+                    for i, target_name in enumerate(model_config["targets"].split(',')):
+                        target = allestm.utils.name_to_feature(f'allestm.features.{target_name}')
+
+                        if len(predictions[i].shape) > 2:
+                            preds = predictions[i][0, :dl_seq_data_tmp.shape[0], :]
+                        else:
+                            preds = predictions[i][:dl_seq_data_tmp.shape[0], :]
+
+                        if predictions[i].shape[-1] == 1:
+                            preds = preds.squeeze()
+
+                        if hasattr(target, 'inverse_transform'):
+                            preds = target.inverse_transform(preds)
+
+                        results.setdefault(target_name, {}).setdefault(method, {}).setdefault(model_config['fold'], preds.tolist())
+
     # RF.
     if do_rf:
         for target_group in config['methods']['rf']:
@@ -102,15 +147,13 @@ def main():
                 else:
                     predictions = model.predict_proba(data)
 
-                print(predictions)
-                
                 if isinstance(target, allestm.features.binary.Binary):
                     predictions = predictions[:, 1]
 
                 if hasattr(target, 'inverse_transform'):
                     predictions = target.inverse_transform(predictions)
 
-                print(predictions)
+                results.setdefault(model_config['targets'], {}).setdefault('rf', {}).setdefault(model_config['fold'], predictions.tolist())
 
     # XGB.
     if do_xgb:
@@ -134,66 +177,49 @@ def main():
                 if hasattr(target, 'inverse_transform'):
                     predictions = target.inverse_transform(predictions)
 
-                print(predictions)
+                results.setdefault(model_config['targets'], {}).setdefault('xgb', {}).setdefault(model_config['fold'], predictions.tolist())
 
-    # DL.
-    if do_dl:
-        def run_dl(seq, pssm, model_file, queue):
-            model = keras.models.load_model(model_file)
-            predictions = model.predict([dl_seq_data, dl_pssm_data])
-            queue.put(predictions)
+    with open('tmp.json', 'r') as fh:
+        results = json.load(fh)
 
-        dl_seq_data_tmp, dl_pssm_data_tmp = (x.to_numpy() for x in prepare_data(seq, pssm, window=0))
+# ["lstm_", "cnn_", "dcnn_", "rf_", "xgb_"]
+# * 5 folds
+# blending
+# avg
+# x targets
 
-        dl_seq_data = np.zeros((1, config['config']['max_length'], len(seq_transformer)))
-        dl_pssm_data = np.zeros((1, config['config']['max_length'], len(seq_transformer)))
 
-        dl_seq_data[0, :dl_seq_data_tmp.shape[0], :dl_seq_data_tmp.shape[1]] = dl_seq_data_tmp
-        dl_pssm_data[0, :dl_pssm_data_tmp.shape[0], :dl_pssm_data_tmp.shape[1]] = dl_pssm_data_tmp
-
-        for method in ['cnn', 'dcnn', 'lstm']:
-            for target_group in config['methods'][method]:
-                log.debug(f'Target group {target_group}.')
-
-                queue = multiprocessing.SimpleQueue()
-                for model_config in config['methods'][method][target_group]:
-                    log.debug(f'Model config: {model_config}.')
-
-                    model_file = f'models/{model_config["model_file"]}'
-                    log.debug(f'Loading model file {model_file} for targets {model_config["targets"]}.')
-
-                    # Necessary to free memory after prediction.
-                    p = multiprocessing.Process(target=run_dl, args=(dl_seq_data, dl_pssm_data, model_file, queue))
-                    p.start()
-                    p.join()
-
-                    predictions = queue.get()
-                    print([x.shape for x in predictions])
-                    print(predictions)
-                    for i, target_name in enumerate(model_config["targets"].split(',')):
-                        target = allestm.utils.name_to_feature(f'allestm.features.{target_name}')
-
-                        if len(predictions[i].shape) > 2:
-                            preds = predictions[i][0, :dl_seq_data_tmp.shape[0], :]
-                        else:
-                            preds = predictions[i][:dl_seq_data_tmp.shape[0], :]
-
-                        if predictions[i].shape[-1] == 1:
-                            preds = preds.squeeze()
-
-                        if hasattr(target, 'inverse_transform'):
-                            preds = target.inverse_transform(preds)
-
-                        print(target)
-                        print(preds)
-
-                    break
-            break
 
     # Blending
+    if do_blending:
+        for target_group in config['methods']['blending']:
+            log.debug(f'Target group {target_group}.')
+
+            for model_config in config['methods']['blending'][target_group]:
+                log.debug(f'Model config: {model_config}.')
+                data = np.column_stack([np.array(results[model_config['targets']][method][str(model_config['fold'])]) for method in ['cnn', 'dcnn', 'lstm', 'rf', 'xgb']])
+
+                target = allestm.utils.name_to_feature(f'allestm.features.{model_config["targets"]}')
+
+                model_file = f'models/{model_config["model_file"]}'
+                log.debug(f'Loading model file {model_file} for target {target}.')
+                model = joblib.load(model_file)
+
+                if isinstance(model, LinearRegression):
+                    predictions = model.predict(data)
+                else:
+                    predictions = model.predict_proba(data)
+
+                results.setdefault(model_config['targets'], {}).setdefault('blending', {}).setdefault(model_config['fold'], predictions.tolist())
 
     # Avg
+    if do_avg:
+        for target in results:
+            avg = np.sum([results[target]['blending'][x] for x in results[target]['blending']], axis=0)
+            avg /= len(results[target]['blending'])
+            results.setdefault(target, {}).setdefault('avg', avg.tolist())
 
+    print(results)
 
 if __name__ == '__main__':
     main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='allestm',
+      version='1.0',
+      description='Predicting various structural features of transmembrane proteins.',
+      url='http://github.com/phngs/allestm',
+      author='Peter Hönigschmid',
+      author_email='hoenigschmid.peter@gmail.com',
+      license='MIT',
+      packages=['allestm'],
+      zip_safe=False)
